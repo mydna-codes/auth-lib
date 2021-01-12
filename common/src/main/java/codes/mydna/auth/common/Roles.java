309313e109a0cfb7fa2734a9ae96265434dfe175
MusicDin <dm6067@student.uni-lj.si>
2021-01-13 00:22:16 +0100

    Fixed roles visibility

--- common/src/main/java/codes/mydna/auth/common/Roles.java
+++ common/src/main/java/codes/mydna/auth/common/Roles.java
@@ -2,11 +2,11 @@ package codes.mydna.auth.common;
 
 public class Roles {
 
-    static class GeneralRole {
-        static final String GUEST = "mdc_guest";
+    public static class GeneralRole {
+        public static final String GUEST = "mdc_guest";
     }
 
-    static class KeycloakRealmRole {
+    public static class KeycloakRealmRole {
         public static final String ADMIN = "mdc_admin";
         public static final String PRO_USER = "mdc_pro_user";
         public static final String USER = "mdc_user";
